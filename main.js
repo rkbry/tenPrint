f2d328f6f3b32c284aae2ea59e7f59333f812836
R. Bryant <rkbry@outlook.com>
2017-01-09 20:47:41 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+/**
+ * 10 PRINT CHR$(205.5+RND(1)); : GOTO 10
+ **/
+function tenPrint(cols, rows) {
+  var maze = '';
+  for (var y = 0; y < rows; y++) {
+    for (var x = 0; x < cols; x++) {
+      maze += (Math.random() > 0.5) ? '╱' : '╲';
+    }
+    maze += '\n';
+  }
+  return maze;
+}
